ccf22f7530f843d5707728c7a600778389f3431c
Omarmouss <gusmouom@student.gu.se>
2024-10-20 15:25:27 +0200

    everything

--- scenes/color_rect.gd
+++ scenes/color_rect.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	color = Color(color.r, color.g, color.b, color.a+0.5*delta)
 	if color.a > 0.99:
-		get_tree().change_scene_to_file("res://all/scenes/game.tscn")
+		get_tree().change_scene_to_file("res://scenes/level1/level_1.tscn")
 
 
 func _on_text_exposition_next_scene() -> void:
